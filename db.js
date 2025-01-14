12502d9b68a1a84ce83c22075730e6e483b82606
MhkMafia <mhk1mafia123@gmail.com>
2025-01-14 12:25:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST || 'dpg-cu30gpogph6c73bl9oo0-a',
+    user: process.env.DB_USER || 'bus_reservation_database_user',
+    password: process.env.DB_PASSWORD || 'iPpsvaEpCdx8d66YiwN8LpNWf30ovhn9',
+    database: process.env.DB_NAME || 'bus_reservation_database',
+});
+
+module.exports = db;
 
