80b76cc97d740dd58bc58d041029fd4f5a4a4db4
MhkMafia <mhk1mafia123@gmail.com>
2025-01-14 11:55:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'bus_reservation',
+    host: process.env.DB_HOST || 'postgres.railway.internal',
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'KEjrhoIgxyThUqhhriHiXvPuTTeCUewf',
+    database: process.env.DB_NAME || 'tables',
 });
 
 module.exports = db;
